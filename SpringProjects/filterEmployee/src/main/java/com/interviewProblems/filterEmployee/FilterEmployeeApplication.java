a8261d0fcda653167908b4eef2c0b435e9677e94
Rohit Mahendran <rohatwork@outlook.com>
2024-04-02 13:06:04 +0530

    employee sorted according to the problem statement

--- SpringProjects/filterEmployee/src/main/java/com/interviewProblems/filterEmployee/FilterEmployeeApplication.java
+++ SpringProjects/filterEmployee/src/main/java/com/interviewProblems/filterEmployee/FilterEmployeeApplication.java
@@ -7,19 +7,38 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FilterEmployeeApplication {
 
 	public static void main(String[] args) {
 
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(FilterEmployeeApplication.class, args);
-		//Employee e1 = applicationContext.getBean(Employee.class, "Fname1","Lname1",1000.00,101);
+
+		//ToDo: find a way to assign values to applicationContext bean in AllArgsConstructor style
+		Employee e1 = applicationContext.getBean(Employee.class);
+		e1.setFirstName("FName1");
+		e1.setLastName("LName1");
+		e1.setSalary(50000.50F);
+		e1.setDeptId((short)101);
 		Employee e2 = applicationContext.getBean(Employee.class);
+		Employee e3 = new Employee("FName2","LName2",45000F, (short)103);
+		Employee e4 = new Employee("FName3","LName3",47000F, (short)105);
+		Employee e5 = new Employee("FName4","LName4",44000F, (short)105);
+		Employee e6 = new Employee("FName5","LName4",45000F, (short)107);
 
+		List<Employee> employees = new ArrayList<Employee>();
+		employees.add(e1);
+		employees.add(e2);
+		employees.add(e3);
+		employees.add(e4);
+		employees.add(e5);
+		employees.add(e6);
 
-		/*Employee e3 = applicationContext.getBean(Employee.class);
-		Employee e4 = applicationContext.getBean(Employee.class);*/
-		System.out.println(e2);
+		List<Employee> sortedEmployees = employeeSorter(employees);
+		sortedEmployees.forEach(System.out::println);
 	}
 
 	@Bean
@@ -27,4 +46,15 @@ public class FilterEmployeeApplication {
 		return new Employee();
 	}
 
+	public static List<Employee> employeeSorter(List<Employee> employeeList){
+		Comparator<Employee> salaryDesc = Comparator.comparing(Employee::getSalary);
+
+		List<Employee> SortedEmployees = employeeList.stream()
+				.filter(e->e.getDeptId()==(short) 105)
+				.sorted(Collections.reverseOrder(salaryDesc))
+				//.forEach(System.out::println); //Terminal operator which returns void,so no ops to be used after this
+				.toList();
+		return SortedEmployees;
+
+	}
 }
--- SpringProjects/filterEmployee/src/main/java/model/Employee.java
+++ SpringProjects/filterEmployee/src/main/java/model/Employee.java
@@ -1,14 +1,14 @@
 package model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @ToString
 @Component
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Employee {
     private String firstName;
